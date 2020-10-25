a24aafc3fadfe0972080b77a1fdc87382fb9771e
pavelsitnikov <pavelsitn@gmail.com>
2020-10-25 21:45:23 +0300

    wip:svm

--- homeworks_basic/assignment0_03_SVM/svm.py
+++ homeworks_basic/assignment0_03_SVM/svm.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-def rbf(x_1, x_2, sigma=1.):
+def rbf(x_1, x_2, sigma = 1.):
     '''Computes rbf kernel for batches of objects
 
     Args:
@@ -17,15 +17,18 @@ def rbf(x_1, x_2, sigma=1.):
         kernel function values for all pairs of samples from x_1 and x_2
         torch.tensor of type torch.float32 shaped `(#samples_1, #samples_2)`
     '''
-    distances = ### YOUR CODE HERE
+    ### YOUR CODE HERE
+    gamma = 1 / 2 / sigma ** 2
+    distances = torch.exp(-gamma * torch.abs(x_1 - x_2))
     return torch.Tensor(distances).type(torch.float32)
+
 
 def hinge_loss(scores, labels):
     '''Mean loss for batch of objects
     '''
     assert len(scores.shape) == 1
     assert len(labels.shape) == 1
-    return ### YOUR CODE HERE
+    return torch.max(torch.zeros(),1 - labels @ scores)
 
 
 class SVM(BaseEstimator, ClassifierMixin):
@@ -40,16 +43,16 @@ class SVM(BaseEstimator, ClassifierMixin):
             kernel function values for all pairs of samples from x_1 and x_2
             torch.tensor shaped `(#samples_1, #samples_2)` of type torch.float32
         '''
-        return ### YOUR CODE HERE
-    
+        return torch.Tensor(torch.abs(x_1 - x_2)).type(torch.float32)
+
     def __init__(
-        self,
-        lr: float=1e-3,
-        epochs: int=2,
-        batch_size: int=64,
-        lmbd: float=1e-4,
-        kernel_function=None,
-        verbose: bool=False,
+            self,
+            lr: float = 1e-3,
+            epochs: int = 2,
+            batch_size: int = 64,
+            lmbd: float = 1e-4,
+            kernel_function=None,
+            verbose: bool = False,
     ):
         self.lr = lr
         self.epochs = epochs
@@ -69,27 +72,27 @@ class SVM(BaseEstimator, ClassifierMixin):
         K = self.kernel_function(X, X).float()
 
         self.betas = torch.full((n_obj, 1), fill_value=0.001, dtype=X.dtype, requires_grad=True)
-        self.bias = torch.zeros(1, requires_grad=True) # I've also add bias to the model
-        
+        self.bias = torch.zeros(1, requires_grad=True)  # I've also add bias to the model
+
         optimizer = optim.SGD((self.betas, self.bias), lr=self.lr)
         for epoch in range(self.epochs):
             perm = torch.randperm(n_obj)  # Generate a set of random numbers of length: sample size
-            sum_loss = 0.                 # Loss for each epoch
+            sum_loss = 0.  # Loss for each epoch
             for i in range(0, n_obj, self.batch_size):
                 batch_inds = perm[i:i + self.batch_size]
-                x_batch = X[batch_inds]   # Pick random samples by iterating over random permutation
-                y_batch = Y[batch_inds]   # Pick the correlating class
+                x_batch = X[batch_inds]  # Pick random samples by iterating over random permutation
+                y_batch = Y[batch_inds]  # Pick the correlating class
                 k_batch = K[batch_inds]
-                
-                optimizer.zero_grad()     # Manually zero the gradient buffers of the optimizer
-                
-                preds = ### YOUR CODE HERE # get the matrix product using SVM parameters: self.betas and self.bias
+
+                optimizer.zero_grad()  # Manually zero the gradient buffers of the optimizer
+
+                preds =  ### YOUR CODE HERE # get the matrix product using SVM parameters: self.betas and self.bias
                 preds = preds.flatten()
                 loss = self.lmbd * self.betas[batch_inds].T @ k_batch @ self.betas + hinge_loss(preds, y_batch)
-                loss.backward()           # Backpropagation
-                optimizer.step()          # Optimize and adjust weights
+                loss.backward()  # Backpropagation
+                optimizer.step()  # Optimize and adjust weights
 
-                sum_loss += loss.item()   # Add the loss
+                sum_loss += loss.item()  # Add the loss
 
             if self.verbose: print("Epoch " + str(epoch) + ", Loss: " + str(sum_loss / self.batch_size))
 
@@ -102,7 +105,7 @@ class SVM(BaseEstimator, ClassifierMixin):
             batch = torch.from_numpy(batch).float()
             K = self.kernel_function(batch, self.X)
             # compute the margin values for every object in the batch
-            return ### YOUR CODE HERE
+            return  ### YOUR CODE HERE
 
     def predict(self, batch):
         scores = self.predict_scores(batch)
